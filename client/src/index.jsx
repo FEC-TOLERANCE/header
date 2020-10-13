3bdf8c97dfbd7dcdbc21b003ec16fbb3da7420ee
James Stockman <jamesstockman14@gmail.com>
2020-10-13 10:24:05 -0700

    react scaffolding setup, forgor to save in previous commit

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      itemId: 1
+    };
+    this.getItemId = this.getItemId.bind(this);
+  }
+
+  getItemId() {
+    //query url to find id
+    this.setState(ItemId: );
+  }
+
+  componentDidMount() {
+    this.getItemId();
+  }
 
 
-ReactDOM.render(
-  <React.StrictMode>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  render() {
+    return (
+      <div>
+        <div className="panel">
+          <h1>Kickstarter Header</h1>
+          <nav className="nav">
+            <span>
+              Funding Goal
+            </span>
+            <span>
+              Amount Funded
+            </span>
+          </nav>
+          <div/>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
 
 
 
